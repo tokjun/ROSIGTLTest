73e585897bb1be2180a08c51cf8ec59622577ca2
Junichi Tokuda <tokuda@bwh.harvard.edu>
2017-01-06 15:30:24 -0500

    Time stamping, data size configuration.

--- SendServer.cxx
+++ SendServer.cxx
@@ -27,9 +27,10 @@ void SendTransform(igtl::Socket* socket);
 void SendPoint(igtl::Socket* socket, int n);
 void SendImage(igtl::Socket* socket, int len);
 void SendString(igtl::Socket* socket, int len);
-  
-void GetRandomTestMatrix(igtl::Matrix4x4& matrix);
+
+void GetRandomTestMatrix(igtl::Matrix4x4& matrix, bool positionOnly=false);
 void GetRandomString(int size, std::string& str);
+void GetRandomImageInt8(int size, char* ptr);
 
 int main(int argc, char* argv[])
 {
@@ -51,7 +52,7 @@ int main(int argc, char* argv[])
 
   int    port     = atoi(argv[1]);
   double fps      = atof(argv[2]);
-  int    interval = (int) (1000.0 / fps);
+  double interval = (double) (1000.0 / fps);
   const char * type = argv[3];
   int    size     = 1;
   int    nIter    = atoi(argv[4]);
@@ -71,7 +72,12 @@ int main(int argc, char* argv[])
     }
 
   igtl::Socket::Pointer socket;
-  
+
+  // Time stamp to control the interval
+  igtl::TimeStamp::Pointer ts;
+  ts = igtl::TimeStamp::New();
+  ts->GetTime();
+
   while (1)
     {
     //------------------------------------------------------------
@@ -84,6 +90,9 @@ int main(int argc, char* argv[])
       // loop
       for (int i = 0; i < nIter; i ++)
         {
+        ts->GetTime();
+        double start = ts->GetTimeStamp();
+
         if (strcmp(type, "TRANSFORM") == 0)
           {
           SendTransform(socket);
@@ -100,7 +109,25 @@ int main(int argc, char* argv[])
           {
           SendString(socket, size);
           }
-        igtl::Sleep(interval); // wait
+        else
+          {
+          std::cerr << "Wrong type is specified. " << std::endl;
+          socket->CloseSocket();
+          break;
+          }
+        
+        ts->GetTime();
+        double end = ts->GetTimeStamp();
+        double elapsed = (end - start) * 1000.0; // ms
+        double remain = interval - elapsed; // ms
+        if (remain < 0.0)
+          {
+          std::cerr << "[WARMING] Sending delay: " << remain << std::endl;
+          }
+        else
+          {
+          igtl::Sleep((int)(remain)); // wait
+          }
         }
       }
     }
@@ -114,6 +141,10 @@ int main(int argc, char* argv[])
 
 void SendTransform(igtl::Socket* socket)
 {
+  igtl::TimeStamp::Pointer ts;
+  ts = igtl::TimeStamp::New();
+  ts->GetTime();
+
   igtl::TransformMessage::Pointer transMsg;
   transMsg = igtl::TransformMessage::New();
   transMsg->SetDeviceName("Tracker");
@@ -121,54 +152,43 @@ void SendTransform(igtl::Socket* socket)
   igtl::Matrix4x4 matrix;
   GetRandomTestMatrix(matrix);
   transMsg->SetMatrix(matrix);
+  transMsg->SetTimeStamp(ts);
   transMsg->Pack();
   socket->Send(transMsg->GetPackPointer(), transMsg->GetPackSize());
 }
 
 void SendPoint(igtl::Socket* socket, int n)
 {
+  igtl::Matrix4x4 matrix;
+
+  igtl::TimeStamp::Pointer ts;
+  ts = igtl::TimeStamp::New();
+  ts->GetTime();
+
   igtl::PointMessage::Pointer pointMsg;
   pointMsg = igtl::PointMessage::New();
   pointMsg->SetDeviceName("PointSender");
 
-  //---------------------------
-  // Create 1st point
-  igtl::PointElement::Pointer point0;
-  point0 = igtl::PointElement::New();
-  point0->SetName("POINT_0");
-  point0->SetGroupName("GROUP_0");
-  point0->SetRGBA(0xFF, 0x00, 0x00, 0xFF);
-  point0->SetPosition(10.0, 20.0, 30.0);
-  point0->SetRadius(15.0);
-  point0->SetOwner("IMAGE_0");
-  
-  //---------------------------
-  // Create 2nd point
-  igtl::PointElement::Pointer point1;
-  point1 = igtl::PointElement::New();
-  point1->SetName("POINT_1");
-  point1->SetGroupName("GROUP_0");
-  point1->SetRGBA(0x00, 0xFF, 0x00, 0xFF);
-  point1->SetPosition(40.0, 50.0, 60.0);
-  point1->SetRadius(45.0);
-  point1->SetOwner("IMAGE_0");
-  
-  //---------------------------
-  // Create 3rd point
-  igtl::PointElement::Pointer point2;
-  point2 = igtl::PointElement::New();
-  point2->SetName("POINT_2");
-  point2->SetGroupName("GROUP_0");
-  point2->SetRGBA(0x00, 0x00, 0xFF, 0xFF);
-  point2->SetPosition(70.0, 80.0, 90.0);
-  point2->SetRadius(75.0);
-  point2->SetOwner("IMAGE_0");
-  
+  for (int i = 0; i < n; i ++)
+    {
+    //---------------------------
+    // Create point
+    GetRandomTestMatrix(matrix, true);
+
+    igtl::PointElement::Pointer point0;
+    point0 = igtl::PointElement::New();
+    point0->SetName("POINT_0");
+    point0->SetGroupName("GROUP_0");
+    point0->SetRGBA(0xFF, 0x00, 0x00, 0xFF);
+    point0->SetPosition(matrix[0][3], matrix[1][3], matrix[2][3]);
+    point0->SetRadius(15.0);
+    point0->SetOwner("IMAGE_0");
+    pointMsg->AddPointElement(point0);
+    }
+
   //---------------------------
   // Pack into the point message
-  pointMsg->AddPointElement(point0);
-  pointMsg->AddPointElement(point1);
-  pointMsg->AddPointElement(point2);
+  pointMsg->SetTimeStamp(ts);
   pointMsg->Pack();
   
   //------------------------------------------------------------
@@ -179,6 +199,10 @@ void SendPoint(igtl::Socket* socket, int n)
 
 void SendImage(igtl::Socket* socket, int len)
 {
+  igtl::TimeStamp::Pointer ts;
+  ts = igtl::TimeStamp::New();
+  ts->GetTime();
+
   //------------------------------------------------------------
   // size parameters
   int   size[]     = {256, 256, 1};       // image dimension
@@ -186,6 +210,9 @@ void SendImage(igtl::Socket* socket, int len)
   int   svsize[]   = {256, 256, 1};       // sub-volume size
   int   svoffset[] = {0, 0, 0};           // sub-volume offset
   int   scalarType = igtl::ImageMessage::TYPE_UINT8;// scalar type
+
+  size[1] = len / (256*4);
+  size[2] = 4;
   
   //------------------------------------------------------------
   // Create a new IMAGE type message
@@ -200,12 +227,15 @@ void SendImage(igtl::Socket* socket, int len)
   //------------------------------------------------------------
   // Set image data (See GetTestImage() bellow for the details)
   //GetTestImage(imgMsg, filedir, i % 5);
+  GetRandomImageInt8((int)imgMsg->GetImageSize(), (char*)imgMsg->GetScalarPointer());
   
   //------------------------------------------------------------
   // Get random orientation matrix and set it.
   igtl::Matrix4x4 matrix;
   GetRandomTestMatrix(matrix);
   imgMsg->SetMatrix(matrix);
+
+  imgMsg->SetTimeStamp(ts);
   
   //------------------------------------------------------------
   // Pack (serialize) and send
@@ -216,6 +246,9 @@ void SendImage(igtl::Socket* socket, int len)
 
 void SendString(igtl::Socket* socket, int len)
 {
+  igtl::TimeStamp::Pointer ts;
+  ts = igtl::TimeStamp::New();
+  ts->GetTime();
   
   //------------------------------------------------------------
   // Allocate Transform Message Class
@@ -227,11 +260,12 @@ void SendString(igtl::Socket* socket, int len)
   stringMsg = igtl::StringMessage::New();
   stringMsg->SetDeviceName("StringMessage");
   stringMsg->SetString(str.c_str());
+  stringMsg->SetTimeStamp(ts);
   stringMsg->Pack();
   socket->Send(stringMsg->GetPackPointer(), stringMsg->GetPackSize());
 }
 
-void GetRandomTestMatrix(igtl::Matrix4x4& matrix)
+void GetRandomTestMatrix(igtl::Matrix4x4& matrix, bool positionOnly)
 {
   float position[3];
   float orientation[4];
@@ -243,6 +277,12 @@ void GetRandomTestMatrix(igtl::Matrix4x4& matrix)
   position[2] = 50.0 * cos(phi);
   phi = phi + 0.2;
 
+  if (positionOnly)
+    {
+    return;
+    }
+    
+  
   // random orientation
   static float theta = 0.0;
   orientation[0]=0.0;
@@ -258,7 +298,7 @@ void GetRandomTestMatrix(igtl::Matrix4x4& matrix)
   matrix[1][3] = position[1];
   matrix[2][3] = position[2];
   
-  igtl::PrintMatrix(matrix);
+  //igtl::PrintMatrix(matrix);
 }
 
 void GetRandomString(int size, std::string& str)
@@ -270,5 +310,16 @@ void GetRandomString(int size, std::string& str)
   for (int i = 0; i < size; i ++)
     {
       str += alphabets[rand() % nalphabets];
+    }
+}
+
+void GetRandomImageInt8(int size, char* ptr)
+{
+  char* p = ptr;
+  char* ptr_end = p + size;
+  while (p < ptr_end)
+    {
+    *p = rand() % 255;
+    p++;
     }
 }
